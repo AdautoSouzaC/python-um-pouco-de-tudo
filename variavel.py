1a8c5ae2d11e5cef5868ab8b22e834051296106a
adautosouzaC <dautosouza1005@gmail.com>
2024-01-04 16:38:56 -0300

    Exe. de variaveis

---
+++ variavel.py
@@ -0,0 +1,22 @@
+media = 1
+
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = "Adauto Souza", 40
+estado = True
+
+# Função type
+print(type(media))
+print(type(nome))
+print(type(idade))
+print(type(estado))
+print(type(1+2j))
+
+#Função isinstance()
+a= 26
+b = "lua"
+print(isinstance(a, (int, float)))
+
+a = 15
+c = 10
+r = a * c
+print(r)
