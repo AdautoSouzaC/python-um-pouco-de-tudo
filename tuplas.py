ca840753e8f7fb2bfa56070214a57369e6e70523
adautosouzaC <dautosouza1005@gmail.com>
2024-03-03 16:50:24 -0300

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,22 @@
+'''tuplas = (2,4,6,8,10)
+print(tuplas)'''
+
+'''haleogenios = ('F', 'Cl', 'Br','I', 'At')
+gases_nobres = ('He', 'Ne', "Ar", 'Xe', 'Kr', 'Rn')
+elementos = haleogenios + gases_nobres
+t1 = (2,4,9,8,4,5,6,2,8)
+print(t1.count(8))'''
+
+'''haleogenios = ('F', 'Cl', 'Br','I', 'At')
+gases_nobres = ('He', 'Ne', "Ar", 'Xe', 'Kr', 'Rn')
+elementos = haleogenios + gases_nobres
+t1 = (2,4,9,8,4,5,6,2,8)
+print(haleogenios[0:2])
+print(gases_nobres[0:4])'''
+
+haleogenios = ('F', 'Cl', 'Br','I', 'At')
+gases_nobres = ('He', 'Ne', "Ar", 'Xe', 'Kr', 'Rn')
+elementos = haleogenios + gases_nobres
+t1 = (2,4,9,8,4,5,6,2,8)
+print('Cl' in haleogenios)
+print('He' in gases_nobres)
